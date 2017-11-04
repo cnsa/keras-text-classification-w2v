cbf32dd5bd92b1daeedb20005e7009effab6f40b
Alexander Merkulov <sasha@merqlove.ru>
2017-11-04 15:57:30 +0300

    Fix formatting.

--- en_train.py
+++ en_train.py
@@ -18,7 +18,6 @@ random.seed(1000)
 # Newsline folder and format
 data_folder = data_folder_path()
 
-
 # Word2Vec number of features
 num_features = 500
 # Limit each newsline to a fixed number of words
@@ -35,21 +34,19 @@ category_data = get_category_data(data_folder)
 document_X, document_Y, news_categories = stream_reuters_documents(category_data, columns=['Name', 'Type', 'Newslines'],
                                                                    selected_categories=selected_categories)
 
-
 # Tokenized document collection
 newsline_documents, number_of_documents = tokenize_documents(document_X, document_Y)
 
 # Create new Gensim Word2Vec model
-X, Y, num_categories = word_to_vec(newsline_documents, number_of_documents, document_Y,selected_categories,
+X, Y, num_categories = word_to_vec(newsline_documents, number_of_documents, document_Y, selected_categories,
                                    data_folder, model_name=word2vec_model_name, num_features=num_features,
                                    document_max_num_words=document_max_num_words)
-
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3)
 
 model = Sequential()
 
-model.add(LSTM(int(document_max_num_words*1.5), input_shape=(document_max_num_words, num_features)))
+model.add(LSTM(int(document_max_num_words * 1.5), input_shape=(document_max_num_words, num_features)))
 model.add(Dropout(0.3))
 model.add(Dense(num_categories))
 model.add(Activation('sigmoid'))
--- train.py
+++ train.py
@@ -19,7 +19,6 @@ random.seed(1000)
 # Newsline folder and format
 data_folder = data_folder_path('cnsa')
 
-
 # Word2Vec number of features
 num_features = 500
 # Limit each newsline to a fixed number of words
@@ -36,22 +35,20 @@ x_test_title = df.Title
 mlb = MultiLabelBinarizer()
 document_Y = dict(enumerate(mlb.fit_transform(y_train_text)))
 
-
 # Tokenized document collection
 newsline_documents, number_of_documents = tokenize_documents(document_X, document_Y,
                                                              lang='russian', regex=u'[А-Яа-яЁa-zA-Z\']+')
 
 # Create new Gensim Word2Vec model
-X, Y, num_categories = word_to_vec(newsline_documents, number_of_documents, document_Y,selected_categories,
+X, Y, num_categories = word_to_vec(newsline_documents, number_of_documents, document_Y, selected_categories,
                                    data_folder, model_name=word2vec_model_name, num_features=num_features,
                                    document_max_num_words=document_max_num_words)
-
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3)
 
 model = Sequential()
 
-model.add(LSTM(int(document_max_num_words*1.5), input_shape=(document_max_num_words, num_features)))
+model.add(LSTM(int(document_max_num_words * 1.5), input_shape=(document_max_num_words, num_features)))
 model.add(Dropout(0.3))
 model.add(Dense(num_categories))
 model.add(Activation('sigmoid'))

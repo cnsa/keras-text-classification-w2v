71a450a3bd1d43289eeea33ccd259e03d8fe5a25
Alexander Merkulov <sasha@merqlove.ru>
2017-11-14 14:20:28 +0300

    More graphs

--- reval.py
+++ reval.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-from numpy import random, arange, float32, ndenumerate
+from numpy import random, arange, float32, ndenumerate, tile, repeat, c_
 
 import h5py, os
 
@@ -51,31 +51,58 @@ for (x, y), value in ndenumerate(predicted[:100]):
 scatter_matrix(pd.DataFrame(predicted[:10]), alpha=0.2, figsize=(6, 6))
 scatter_matrix(pd.DataFrame(Y_test[:10]), alpha=0.2, figsize=(6, 6))
 
-p1 = pd.DataFrame(predicted[:10]).plot(title=u"Предсказанные результаты")
-p1.set_xlabel('Text')
-p1.set_ylabel('Probability')
-p2 = pd.DataFrame(predicted[:10]).plot(title=u"Предсказанные результаты", kind="box")
-p2.set_xlabel('Factor')
-p2.set_ylabel('Probability')
 
-p3 = pd.DataFrame(Y_test[:10]).plot(title=u"Актуальные результаты")
-p3.set_xlabel('Text')
-p3.set_ylabel('Probability')
-p4 = pd.DataFrame(Y_test[:10]).plot(title=u"Актуальные результаты", kind="box")
-p4.set_xlabel('Factor')
-p4.set_ylabel('Probability')
+def probability_plot(data, title=None, x_title=None, y_title=None, kind="line"):
+    p = pd.DataFrame(data).plot(title=title, kind=kind)
+    p.set_xlabel(x_title)
+    p.set_ylabel(y_title)
 
-df = pd.DataFrame(predicted[:100])
-df['Factor'] = pd.Series(range(predicted.shape[1]))
+
+probability_plot(predicted[:10], u"Предсказанные результаты", 'Text', 'Probability')
+probability_plot(predicted[:10], u"Предсказанные результаты", 'Factor', 'Probability', kind="box")
+
+probability_plot(Y_test[:10], u"Актуальные результаты", 'Text', 'Probability')
+probability_plot(Y_test[:10], u"Актуальные результаты", 'Factor', 'Probability', kind="box")
+
 sns.set(style="ticks")
-sns.pairplot(df, hue="Factor")
 
-df = pd.DataFrame(Y_test[:100])
-df['Factor'] = pd.Series(range(Y_test.shape[1]))
-sns.set(style="ticks")
-sns.pairplot(df, hue="Factor")
 
-# Show the plot
+def facet_grid(data, limit=None, title="Figure"):
+    if limit is None:
+        size = data.size
+        prob = data.flatten()
+    else:
+        size = limit
+        prob = data[:limit].flatten()
+    shape = data.shape[1]
+    factor = tile(range(shape), size)
+    doc = repeat(range(size), shape)
+    df = pd.DataFrame(c_[prob, factor, doc],
+                      columns=["prob", "factor", "doc"])
+
+    # Initialize a grid of plots with an Axes for each walk
+    grid = sns.FacetGrid(df, col="doc", hue="doc", col_wrap=5, size=1.5)
+
+    # Draw a horizontal line to show the starting point
+    grid.map(plt.axhline, y=0, ls=":", c=".5")
+
+    # Draw a line plot to show the trajectory of each random walk
+    grid.map(plt.plot, "factor", "prob", marker="o", ms=4)
+
+    # Adjust the tick positions and labels
+    grid.set(xticks=arange(shape), yticks=[-3, 3],
+             xlim=(.0, shape), ylim=(0.0, 1.0))
+
+    # Adjust the arrangement of the plots
+    grid.fig.tight_layout(w_pad=1)
+
+    grid.fig.suptitle(title)
+
+
+facet_grid(predicted, limit=100, title="Predicted")
+facet_grid(Y_test, limit=100, title="Actual")
+
+sns.despine(left=True, bottom=True)
 
 # print_predictions(predicted, document_X_title, selected_categories, idx_test, y=y_train_text, show_words=100, encode=False)
 
